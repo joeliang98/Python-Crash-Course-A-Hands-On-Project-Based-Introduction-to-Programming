d2244b6cd5d4927e676e340aa72fa78bde82d4e6
joeliang98 <2387478783@qq.com>
2022-09-20 12:56:09 +0800

    project2 start

--- project1/alien_invasion.py
+++ project1/alien_invasion.py
@@ -1,7 +1,9 @@
 import sys
 import pygame
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 def run_game():
     #初始化游戏，创建一个屏幕对象
@@ -12,14 +14,22 @@ def run_game():
 
     pygame.display.set_caption("Alien Invasion")
     ship = Ship(ai_settings,screen)
+    bullets = Group()
+    aliens = Group()
+    gf.create_fleet(ai_settings,screen,ship,aliens)
+    
+    alien = Alien(ai_settings,screen)
+
+
 
     #开始游戏主循环
     while True:
         #监视键盘和鼠标事件
-        gf.check_events(ship)
+        gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
-        gf.update_screen(ai_settings,screen,ship)
-
+        gf.update_bullets(bullets)
+        
+        gf.update_screen(ai_settings,screen,ship,aliens,bullets)
 run_game()
 
 
--- project1/ship.py
+++ project1/ship.py
@@ -20,7 +20,7 @@ class Ship():
     def update(self):
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.ai_settings.ship_speed_factor
-        if self.moving_left > 0:
+        if self.moving_left and self.rect.left> 0:
             self.center -= self.ai_settings.ship_speed_factor   
     
         self.rect.centerx = self.center
